2ec50ad3377ee6f0b6a89b2ad5a6b32c05a7b9cd
Cléa <cleaportolan@icloud.com>
2025-12-18 16:51:37 +0100

    ajout WC + studio

---
+++ js/wc.js
@@ -0,0 +1,36 @@
+gsap.set(".triangle", {
+  opacity: 0,
+});
+// Animation du texte
+gsap.to(".text-content", {
+  duration: 2,
+  delay: 1,
+  text: "J'en ai passé du temps ici après la pizza du marathon du web... Bref, aucune chance que mon <span class='highlight-yellow'>PC</span> soit ici.",
+  ease: "none",
+  onComplete: function () {
+    // Une fois le texte fini, animer le triangle
+    gsap.to(".triangle", {
+      duration: 0.5,
+      opacity: 1,
+      y: 5,
+      repeat: -1, // répète en boucle
+      yoyo: true, // va-et-vient
+      ease: "sine.inOut",
+    });
+  },
+});
+
+const hint = document.querySelector(".text-typing-wc");
+
+// Masquer au clic
+document.addEventListener("click", function () {
+  hint.style.display = "none";
+});
+
+// Masquer à la touche Entrée
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    // ou event.code === "Enter"
+    hint.style.display = "none";
+  }
+});
